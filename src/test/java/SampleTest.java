7405aae9a56d5b3f2fe30c5254084a1e1e111eb1
MD Dubey <mddubey409@gmail.com>
2021-05-28 11:24:37 +0530

    Sample Test

---
+++ src/test/java/SampleTest.java
@@ -0,0 +1,11 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class SampleTest {
+    @Test
+    void lambdaExpressions() {
+        System.out.println("Hello Wor");
+        assertTrue(true);
+    }
+}
